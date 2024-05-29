ef7cba41f75f8637a2c8a9b2754c307f111c2b85
Julien Nicolas <ju.nicodev@gmail.com>
2024-05-29 12:13:38 +0200

    show listing for stdout version

--- main.go
+++ main.go
@@ -16,16 +16,16 @@ func main() {
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
 		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, e.Request.AbsoluteURL(link))
-		// Visit link found on page
-		// Only those links are visited which are in AllowedDomains
-		//c.Visit(e.Request.AbsoluteURL(link))
+		// link is useful for the hash section for the json version
+		fmt.Println(e.Request.AbsoluteURL(link))
 	})
 
 	// Before making a request print "Visiting ..."
+	/* 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
+	*/
 
 	// Start scraping on https://hackerspaces.org
 	c.Visit("https://news.ycombinator.com")
