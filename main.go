14a1ca100a21abe1fd1e190271920010929ed824
Julien Nicolas <ju.nicodev@gmail.com>
2024-05-29 12:24:00 +0200

    remove query url from results listing

--- main.go
+++ main.go
@@ -5,27 +5,37 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
-func Stdout(e *colly.HTMLElement) {
-	link := e.Attr("href")
-	fmt.Println(e.Request.AbsoluteURL(link))
+func Stdout(url string) func(e *colly.HTMLElement) {
+	return func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		if link != url {
+			fmt.Println(e.Request.AbsoluteURL(link))
+		}
+	}
 }
 
-func Json(e *colly.HTMLElement) {
-	link := e.Attr("href")
-	// Print link
-	// link is useful for the hash section for the json version
-	fmt.Println(e.Request.AbsoluteURL(link))
+func Json(url string) func(e *colly.HTMLElement) {
+	return func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		// link is useful for the hash section for the json version
+		if link != url {
+			fmt.Println(e.Request.AbsoluteURL(link))
+		}
+	}
 }
 
 func main() {
+	const url string = "https://news.ycombinator.com"
+
 	// Instantiate default collector
 	// There is a very useful whitelist option in case
 	// it is decided to recursively crawl URLs
 	c := colly.NewCollector()
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", Stdout)
+	c.OnHTML("a[href]", Stdout(url))
 
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://news.ycombinator.com")
+	c.Visit(url)
 }
