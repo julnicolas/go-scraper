f99f571a1ac82c1838e5d212670854873c277102
Julien Nicolas <ju.nicodev@gmail.com>
2024-05-29 12:16:34 +0200

    refactor code

--- main.go
+++ main.go
@@ -5,27 +5,27 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
+func Stdout(e *colly.HTMLElement) {
+	link := e.Attr("href")
+	fmt.Println(e.Request.AbsoluteURL(link))
+}
+
+func Json(e *colly.HTMLElement) {
+	link := e.Attr("href")
+	// Print link
+	// link is useful for the hash section for the json version
+	fmt.Println(e.Request.AbsoluteURL(link))
+}
+
 func main() {
 	// Instantiate default collector
 	c := colly.NewCollector(
-		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
-		//colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
+	// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+	//colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
 	)
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		// Print link
-		// link is useful for the hash section for the json version
-		fmt.Println(e.Request.AbsoluteURL(link))
-	})
-
-	// Before making a request print "Visiting ..."
-	/* 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-	*/
+	c.OnHTML("a[href]", Stdout)
 
 	// Start scraping on https://hackerspaces.org
 	c.Visit("https://news.ycombinator.com")
