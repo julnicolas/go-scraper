eb105a000aac3061cc157f49f7b70085408d1430
Julien Nicolas <ju.nicodev@gmail.com>
2024-05-29 12:47:49 +0200

    implement cli

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gocolly/colly/v2"
+	"os"
+	"strings"
 )
 
 func Stdout(url string) func(e *colly.HTMLElement) {
@@ -25,8 +28,43 @@ func Json(url string) func(e *colly.HTMLElement) {
 	}
 }
 
+// CLI is a struct reprensenting accepted CLI arguments
+type CLI struct {
+	URL    string
+	Output func(*colly.HTMLElement)
+}
+
+func (o *CLI) Parse() error {
+	var output string
+
+	flag.StringVar(&o.URL, "u", "", "Url to collect links from. Can be used several times")
+	flag.StringVar(&output, "o", "stdout", "Output formats. Possible formats are 'stdout' and 'json'")
+	flag.Parse()
+
+	if o.URL == "" {
+		return fmt.Errorf("cli error - URL argument is empty")
+	}
+
+	switch strings.ToLower(output) {
+	case "stdout":
+		o.Output = Stdout(o.URL)
+
+	case "json":
+		o.Output = Json(o.URL)
+
+	default:
+		return fmt.Errorf("cli error - unsupported format '%s'", output)
+	}
+
+	return nil
+}
+
 func main() {
-	const url string = "https://news.ycombinator.com"
+	cli := &CLI{}
+	if err := cli.Parse(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	// Instantiate default collector
 	// There is a very useful whitelist option in case
@@ -34,8 +72,11 @@ func main() {
 	c := colly.NewCollector()
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", Stdout(url))
+	c.OnHTML("a[href]", cli.Output)
 
 	// Start scraping on https://hackerspaces.org
-	c.Visit(url)
+	if err := c.Visit(cli.URL); err != nil {
+		fmt.Printf("request error - %s\n", err)
+		os.Exit(1)
+	}
 }
